d3e188a223b626ea1ed4e60e4c52fe74b572c9cb
James Miller <james.rex.miller.4@gmail.com>
2020-04-22 05:27:18 -0600

    Add movies model

--- backend/models.py
+++ backend/models.py
@@ -12,3 +12,36 @@ def setup_db(app, database_path=database_path):
   db.app = app
   db.init_app(app)
   db.create_all()
+
+
+class Movies(db.Model):
+  __table__name = "movies"
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(String)
+  actors = Column(String)
+
+  def __init__(self, title, release_date, actors):
+    self.title = title
+    self.release_date = release_date
+    self.actors = actors
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      "id": self.id,
+      "title": self.title,
+      "release_date": self.release_date,
+      "actors": self.actors
+    }
