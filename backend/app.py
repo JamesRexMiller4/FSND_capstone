c3d2b5dd1217f3456fe2ad6fd852d70d0711f375
James Miller <james.rex.miller.4@gmail.com>
2020-04-22 20:53:44 -0600

    Wrap app in create_app method so it can be exported for testing

--- backend/app.py
+++ backend/app.py
@@ -4,23 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from models import setup_db, Movie, Actor, CastMember, db
 
-app = Flask(__name__)
-setup_db(app)
-CORS(app)
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-@app.after_request
-def after_request(response):
-    response.headers.add(
-        "Access-Control-Allow-Headers", "Content-Type, Authorization, true"
-    )
-    response.headers.add(
-        "Access-Control-Allow-Methods", "GET, DELETE, POST, OPTIONS"
-    )
-    response.headers.add("Access-Control-Allow-Credentials", "true")
-    return response
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization, true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET, DELETE, POST, OPTIONS"
+        )
+        response.headers.add("Access-Control-Allow-Credentials", "true")
+        return response
 
-@app.route('/')
-def index():
-    return 'Working'
+    @app.route('/')
+    def index():
+        return 'Working'
 
 
+    return app
